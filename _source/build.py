33d1d72fc60ee8a48e25abc1dd902468eb626eaa
Leonardo Rodriguez <leo@Leonardos-MacBook-Pro.local>
2026-09-23 03:40:55 -0400

    analytics.js despues de core.js para que no se pierda CA.track

--- _source/build.py
+++ _source/build.py
@@ -105,8 +105,8 @@ def scripts(meta):
     tags = [f'<script src="js/i18n/{name}.js"></script>' for name in ["common", *meta.get("i18n", [])]]
     tags += [f'<script src="js/{src}"></script>' for src in meta.get("data", [])]
     tags += [f'<script src="{src}" defer></script>' for src in meta.get("cdn", [])]
-    tags.append('<script src="js/analytics.js"></script>')
     tags.append('<script src="js/core.js"></script>')
+    tags.append('<script src="js/analytics.js"></script>')  # después de core.js: core reasigna window.CA
     tags += [f'<script src="js/{src}"></script>' for src in meta.get("js", [])]
     return "\n" + "\n".join(tags)
 
